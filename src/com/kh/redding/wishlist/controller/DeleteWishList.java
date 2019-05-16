77fe8466222ac5d6470d02a7e2f2de227ffc1701
LJY <LJY@JeongYeon>
2019-05-16 14:55:45 +0900

    버어어어어그으으으

--- src/com/kh/redding/wishlist/controller/DeleteWishList.java
+++ src/com/kh/redding/wishlist/controller/DeleteWishList.java
@@ -19,49 +19,49 @@ import com.kh.redding.wishlist.model.service.WishListService;
 @WebServlet("/delete.wi")
 public class DeleteWishList extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public DeleteWishList() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public DeleteWishList() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		//String배열로 넘어온 걸 ,로 쪼개기
-		String[] list = request.getParameter("deleteList").split(",");
-		System.out.println(request.getParameter("deleteList"));
-		
-		//int배열 생성
-		int[] deleteList = new int[list.length];
-		
-		for(int i=0; i<list.length; i++) {
-			try {
-			deleteList[i] = Integer.parseInt(list[i]);
-			} catch (NumberFormatException nfe) {
-				
-			};
-		}
-
-		
+		int result = 0;
 		//세션으로 로그인 유저 정보 받아오기
 		Member loginUser = (Member) request.getSession().getAttribute("loginUser");
 		int userNum = loginUser.getMno();
-		
-		int result = new WishListService().deleteWishList(deleteList, userNum);
-		
-		String page = "";
-		if(result>0) {
-			response.sendRedirect("/redding/showList.wi?num="+userNum);
+
+		//String배열로 넘어온 걸 ,로 쪼개기
+		if(!request.getParameter("deleteList").equals("")) {
+			String[] list = request.getParameter("deleteList").split(",");
+
+			//int배열 생성
+			int[] deleteList = new int[list.length];
+
+			for(int i=0; i<list.length; i++) {
+				deleteList[i] = Integer.parseInt(list[i]);
+			}
+
+			result = new WishListService().deleteWishList(deleteList, userNum);
+			String page = "";
+			if(result>0) {
+				response.sendRedirect("/redding/showList.wi?num="+userNum);
+			}else {
+				request.setAttribute("msg", "위시리스트 삭제 실패!");
+				request.getRequestDispatcher(page).forward(request, response);
+			}
 		}else {
-			request.setAttribute("msg", "위시리스트 삭제 실패!");
-			request.getRequestDispatcher(page).forward(request, response);
+			response.sendRedirect("/redding/showList.wi?num="+userNum);
 		}
-		
+
+
+
 	}
 
 	/**
