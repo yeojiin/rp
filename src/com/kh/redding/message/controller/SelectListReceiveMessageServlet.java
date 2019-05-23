c8a0b62eba72106272dbefbc16c383787816b0e6
komay <komay@DESKTOP-DN7GUN4>
2019-05-23 21:05:44 +0900

    쪽지 하는중

--- src/com/kh/redding/message/controller/SelectListReceiveMessageServlet.java
+++ src/com/kh/redding/message/controller/SelectListReceiveMessageServlet.java
@@ -2,6 +2,7 @@ package com.kh.redding.message.controller;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,8 +10,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.gson.Gson;
+import com.kh.redding.member.model.vo.Member;
 import com.kh.redding.message.model.service.MessageService;
 import com.kh.redding.message.model.vo.Message;
+import com.kh.redding.product.model.vo.PageInfo;
 
 @WebServlet("/receiveList.mes")
 public class SelectListReceiveMessageServlet extends HttpServlet {
@@ -22,23 +26,65 @@ public class SelectListReceiveMessageServlet extends HttpServlet {
 		//System.out.println("모든 업체들에게 받은 쪽지 전체 리스트 조회 서블릿");
 		
 		int wtype = 20;
-		//System.out.println("wt : " + wtype);
-		
-		ArrayList<Message> receiveList = new MessageService().selectListMes(wtype);
-		
-		//System.out.println("receiveList : " + receiveList);
-		
-		String page = "";
-		
-		if(receiveList != null) {
-			page = "views/admin/a_Message.jsp";
-			request.setAttribute("receiveList", receiveList);
-		}else {
-			page = "views/common/errorPage.jsp";
-			request.setAttribute("msg", "보낸 쪽지 목록 조회에 실패하셨습니다.");
+
+		int listCount = Integer.parseInt(request.getParameter("value"));
+		  
+		int currentPage;
+		int limit;
+		int maxPage;
+		int startPage;
+		int endPage;
+		int startRow;
+		int endRow;
+		  
+		currentPage = 1;
+		if(request.getParameter("currentPage") != null) {
+			currentPage = Integer.parseInt(request.getParameter("currentPage"));
 		}
-		request.getRequestDispatcher(page).forward(request, response);
-	}
+		  
+		limit = 10;
+		maxPage = (int)((double)listCount/ limit + 0.9);
+		  
+		startPage = (((int)((double) currentPage / limit + 0.9)) - 1) * 10 + 1;
+		endPage = startPage + 9 ;
+		  
+		if(maxPage < endPage) {
+			endPage = maxPage;
+		}
+		  
+		PageInfo pi = new PageInfo(currentPage, limit, maxPage, startPage, endPage);
+		  
+		startRow = (pi.getCurrentPage() - 1) * pi.getLimit() + 1;
+		endRow = startRow + pi.getLimit() - 1;
+		  
+		  /*startRow = 1;
+		  endRow=100;*/
+		pi.setStartRow(startRow);
+		pi.setEndRow(endRow);
+		  
+		  //System.out.println("listCount : " + listCount);
+		  //System.out.println("wt : " + wtype);
+		  
+		ArrayList<Message> sendList = new MessageService().selectListMes(pi, wtype);
+		  
+		  //System.out.println("sendList : " + sendList);
+		  
+		HashMap<String, Object> hmap = new HashMap<String, Object>();
+		 
+		ArrayList<Member> cnames = new ArrayList<Member>();
+		cnames = new MessageService().selectCompany();
+		  
+		  
+		  //System.out.println("cnames : " + cnames);
+		  
+		hmap.put("cnames", cnames);
+		hmap.put("sendList",sendList);
+		hmap.put("pi",pi);
+		  
+		  
+		response.setContentType("application/json");
+		new Gson().toJson(hmap, response.getWriter());
+}
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doGet(request, response);
 	}
